ea1d147f4ebe0e1a1f5d0defbde3fe492707c24a
G E <george1015@outlook.com>
2021-08-09 22:34:00 +0100

    Setting up

---
+++ src/main/python/test.py
@@ -0,0 +1,14 @@
+import requests
+import yfinance as yf
+
+# json_test = yf.Ticker("AAPL").info
+# print(json_test)
+
+# test = requests.post("http://localhost:9000/api/test/?format=json",
+#                      headers={
+#                          "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTYyODU0ODA2N30.f_8FkPrQLwMy0DswOcHef-jKFIhv1hJFV_ityGleAXRM86yjULhl47WPXNTpF0Vi-SlTY6raQ5qSfK88xzjWvw",
+#                          'Accept': 'application/json',
+#                          'Content-Type': 'application/json'
+#                      },
+#                      json=json_test)
+# print(test.json())
